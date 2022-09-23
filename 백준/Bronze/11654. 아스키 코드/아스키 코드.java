8d1c684beebb05292b3eaf1cf0751d5b912bd61b
eunbyeol0213 <114052638+eunbyeol0213@users.noreply.github.com>
2022-09-23 11:39:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int b = in.next().charAt(0);
+		//char 형은 int보다 타입이 작아서 담김
+		//자동형변환
+		System.out.println(b);
+	
+	}
+
+}
