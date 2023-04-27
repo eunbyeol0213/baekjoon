22a38772fd2b794bfefef684d76d0bb99648414b
eunbyeol0213 <114052638+eunbyeol0213@users.noreply.github.com>
2023-04-27 17:00:21 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 324 ms, Memory: 18196 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        for(int i=0; i<val; i++){
+            for (int j=0; j<val-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
